2710ed76fa7adfea7ff6438008180a0dc5f5e8b5
Ed <alpezed@gmail.com>
2025-08-13 10:58:05 +0800

    build task card body ui

--- src/components/TaskCard/TaskCardBody.tsx
+++ src/components/TaskCard/TaskCardBody.tsx
@@ -1,3 +1,16 @@
+import { useTaskItemContext } from '../../context/TaskItemContext';
+
 export const TaskCardBody = () => {
-	return <div>TaskCardBody</div>;
+	const { task, formatDate } = useTaskItemContext();
+
+	return (
+		<div className='px-6 py-4'>
+			<div className='text-xs text-gray-500 flex items-center justify-between'>
+				<span>Created: {formatDate(task.createdAt)}</span>
+				{task.updatedAt.getTime() !== task.createdAt.getTime() && (
+					<span>Updated: {formatDate(task.updatedAt)}</span>
+				)}
+			</div>
+		</div>
+	);
 };
